bf18cad103252695bc3e28281d86c1bc8bd3750e
Victor Adu <58883511+aduV24@users.noreply.github.com>
2023-03-17 16:45:26 +0200

    Q1.5
    Added code that increments a counter and displayed on LCD and LEDs

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 // DEFINES AND INCLUDES-------------------------------------------------------|
 
 #define STM32F051												   //COMPULSORY
+#define SW1Pressed (((GPIOA->IDR) & (GPIO_IDR_1))==0)
+#define SW2Pressed (((GPIOA->IDR) & (GPIO_IDR_2))==0)
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
 #include <stdio.h>
@@ -22,22 +24,45 @@ void init_LEDS(void);
 void display_on_LEDs(uint8_t number);
 void init_switches(void);
 
+
 void main(void)
 {
 	init_LCD();
 	init_LEDS();
-
+	init_switches();
+	uint8_t count = 0;
+	display_on_LEDs(count);
 	while(1)
 	{
+		// Check for push button1
+		if(SW1Pressed)
+		{
+			if(count==0xFF); // check that count is not > 225
+			else{
+				count+=1;
+			}
+
+
+		}
+		// Check for push buttton2
+		else if(SW2Pressed){
+			count-=1;
+		}
+		// Display count on LED and LCD
+		display_on_LEDs(count);
+		display_on_LCD(count);
+		delay(70000);
 
 
 
 	}
+
 }
 
 // OTHER FUNCTIONS -----------------------------------------------------------|
 
 void display_on_LCD(uint8_t num){
+	lcd_command(CLEAR);
 	char str[3];
 	sprintf(str,"%d",num); // convert number to string
 	lcd_putstring(str); // put string on LCD
@@ -61,10 +86,8 @@ void display_on_LEDs(uint8_t number){
 
 void init_switches(void){
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN; // Enable Port A
-	GPIOB->MODER &= ~GPIO_MODER_MODER1;// Input mode for SW1
-	GPIOB->MODER &= ~GPIO_MODER_MODER2; // Set to input mode for SW2
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR1_0; // Enable PUPDR for pin1
-	GPIOB->PUPDR |= GPIO_PUPDR_PUPDR2_0; // Enable PUPDR for pin2
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR1_0; // Enable PUPDR for pin1
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR2_0; // Enable PUPDR for pin2
 
 }
 
